602de1f19a662ceefa74fe55780a1f99e7a47ebc
sahilshetye <Killeronloose2606>
2017-05-09 16:23:01 -0500

    Added Setup.py File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Breseq_Extracor',
+    version='1.0.0',
+    packages=['Hansel_HTMLscrapper'],
+    url='www.sahilshetye.com',
+    license='Apache',
+    author='sahilshetye',
+    author_email='sahildevilster@gmail.com',
+    description='Breseq Extractor for  extracting data from HTML output file generated in Breseq'
+)
